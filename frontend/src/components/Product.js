bdba8ab8940e5f289e7f04bb82a4d599894e0716
Rei <reieparei@gmail.com>
2023-06-15 14:21:57 +0200

    changed grid css

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -2,19 +2,18 @@ import React from "react";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 
-const Product = () => {
+const Product = ({ product }) => {
+  const { title, price, category, anime, imageSrc } = product;
+
   return (
-    <Card style={{ width: "18rem" }}>
-      <Card.Img
-        variant="top"
-        src="https://images-na.ssl-images-amazon.com/images/I/51POWVOfprL.jpg"
-      />
+    <Card>
+      <Card.Img variant="top" src={imageSrc} />
       <Card.Body>
-        <Card.Title>Itachi Uchiha Headband</Card.Title>
+        <Card.Title>{title}</Card.Title>
         <Card.Text>
-          <p className="m-0">Price: 20$</p>
-          <p className="m-0">Category: Accessories</p>
-          <p className="m-0">Anime: Naruto Shippuden</p>
+          Price: {price}$ <br />
+          Category: {category} <br />
+          Anime: {anime}
         </Card.Text>
         <Button variant="dark">Add to Cart</Button>
       </Card.Body>
--- frontend/src/pages/Products.js
+++ frontend/src/pages/Products.js
@@ -1,23 +1,76 @@
 import React from "react";
 import Product from "../components/Product";
-import "../styles/products.css";
-import { Container } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
+
+const products = [
+  {
+    id: 1,
+    title: "Itachi Uchiha Headband",
+    price: 20,
+    category: "Accessories",
+    anime: "Naruto Shippuden",
+    imageSrc:
+      "https://images-na.ssl-images-amazon.com/images/I/51POWVOfprL.jpg",
+  },
+  {
+    id: 2,
+    title: "Itachi Uchiha Headband",
+    price: 20,
+    category: "Accessories",
+    anime: "Naruto Shippuden",
+    imageSrc:
+      "https://images-na.ssl-images-amazon.com/images/I/51POWVOfprL.jpg",
+  },
+  {
+    id: 3,
+    title: "Itachi Uchiha Headband",
+    price: 20,
+    category: "Accessories",
+    anime: "Naruto Shippuden",
+    imageSrc:
+      "https://images-na.ssl-images-amazon.com/images/I/51POWVOfprL.jpg",
+  },
+  {
+    id: 4,
+    title: "Itachi Uchiha Headband",
+    price: 20,
+    category: "Accessories",
+    anime: "Naruto Shippuden",
+    imageSrc:
+      "https://images-na.ssl-images-amazon.com/images/I/51POWVOfprL.jpg",
+  },
+  {
+    id: 5,
+    title: "Itachi Uchiha Headband",
+    price: 20,
+    category: "Accessories",
+    anime: "Naruto Shippuden",
+    imageSrc:
+      "https://images-na.ssl-images-amazon.com/images/I/51POWVOfprL.jpg",
+  },
+  {
+    id: 6,
+    title: "Itachi Uchiha Headband",
+    price: 20,
+    category: "Accessories",
+    anime: "Naruto Shippuden",
+    imageSrc:
+      "https://images-na.ssl-images-amazon.com/images/I/51POWVOfprL.jpg",
+  },
+
+  // Add more product objects here
+];
 
 const Products = () => {
   return (
     <Container>
-      <div className="products">
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-      </div>
+      <Row className="grid g-3 ">
+        {products.map((product) => (
+          <Col key={product.id} xs={12} sm={6} md={4} xl={3}>
+            <Product product={product} />
+          </Col>
+        ))}
+      </Row>
     </Container>
   );
 };
